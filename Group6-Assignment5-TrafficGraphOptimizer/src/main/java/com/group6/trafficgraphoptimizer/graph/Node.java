c10629676e273d7923a618451b699711794d99ae
Sushrut Watekar <107336930+SushrutWatekar@users.noreply.github.com>
2025-05-06 15:19:02 +0100

    Update Node.java

--- Group6-Assignment5-TrafficGraphOptimizer/src/main/java/com/group6/trafficgraphoptimizer/graph/Node.java
+++ Group6-Assignment5-TrafficGraphOptimizer/src/main/java/com/group6/trafficgraphoptimizer/graph/Node.java
@@ -1,1 +1,58 @@
 
+package com.group6.trafficgraphoptimizer.graph;
+
+/**
+ * Represents a single intersection or point in the city road network.
+ * Each node is uniquely identified by an ID or name.
+ */
+public class Node {
+    private final String id;
+
+    /**
+     * Creates a new node with the given ID.
+     *
+     * @param id A unique identifier for this node (e.g., "A", "Intersection-1")
+     */
+    public Node(String id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the unique ID of the node.
+     *
+     * @return the node ID
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Custom toString for debug-friendly printing.
+     *
+     * @return Node as string
+     */
+    @Override
+    public String toString() {
+        return id;
+    }
+
+    /**
+     * Two nodes are equal if they share the same ID.
+     *
+     * @param o another object
+     * @return true if same ID
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Node)) return false;
+        Node other = (Node) o;
+        return this.id.equals(other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
+
